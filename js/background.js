2bd3f961bcf5174f20ea30024f07e7ca3b7bebb2
Corentin Smith <corentin.smith@gmail.com>
2014-04-26 18:58:49 +0200

    Fix background and resume link

--- js/background.js
+++ js/background.js
@@ -1,7 +1,16 @@
-// Sorry if I made your browser slow :(
-// I like to play with d3...
+// Yes I like to play with d3.
 
 ;(function() {
+
+console.log("Hello there!");
+
+// get page size
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    pageWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+    pageHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
 var data = [];
 // select a random scale in [50, 200]
@@ -20,6 +29,8 @@ var dims = [];
 text.each(function (d, i) {
     dims.push(text[0][i].getBoundingClientRect());
 });
+
+console.log(dims)
 
 var line = d3.svg.line()
     .x(function (d) { return d.x; })
@@ -71,25 +82,25 @@ function overText(x, y) {
 }
 
 function rand(init, max) {
-    var newval = (0.5 - Math.random()) * scale;
-    newval = Math.max(-scale, Math.min(init + newval, max));
+    var result = (0.5 - Math.random()) * scale;
+    result = Math.max(-scale, Math.min(init + result, max));
     
-    return newval;
+    return result;
 }
 
 function generateData(count) {
-    for (var i = count; i > 0; --i) {
+    for (var i = 0; i < count; i++) {
         pushData();
     }
 }
 
 function pushData() {
-    var newX = rand(x, document.width);
-    var newY = rand(y, document.height);
+    var newX = rand(x, pageWidth);
+    var newY = rand(y, pageHeight);
 
     while (overText(newX, newY)) {
-        newX = rand(x, document.width);
-        newY = rand(y, document.height);
+        newX = rand(x, pageWidth);
+        newY = rand(y, pageHeight);
     }
     x = newX;
     y = newY;
