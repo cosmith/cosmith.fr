78e4a8a0b8018f8bfdf06c87e1521bdd39fe9821
Corentin Smith <corentin@dashdoc.eu>
2022-10-16 15:47:10 +0200

    Add links

--- build.py
+++ build.py
@@ -1,11 +1,23 @@
+import json
 import os
 import shutil
+from functools import cache
 
 
+@cache
 def get_page(filename):
-    with open(filename) as f:
+    with open(f"./pages/{filename}.html") as f:
         return f.read()
 
+@cache
+def get_component(filename):
+    with open(f"./components/{filename}.html") as f:
+        return f.read()
+
+@cache
+def get_data(filename):
+    with open(f"./data/{filename}.json") as f:
+        return json.load(f)
 
 with open("index.html", "r") as index:
     text = index.read()
@@ -13,8 +25,18 @@ with open("index.html", "r") as index:
 shutil.rmtree("build")
 os.makedirs("build")
 shutil.copytree("css", "build/css")
-for filename in ["about", "links"]:
-    page = get_page(f"pages/{filename}.html")
-    out = text.replace("{{page}}", page)
-    with open(f"build/{filename}.html", "w") as f:
+for pagename in ["about", "links"]:
+    page = get_page(pagename)
+    
+    if pagename == "links":
+        link_component = get_component("link")
+        links_data = get_data("links")
+        print((links_data[0]))
+        links = [link_component.format(**link) for link in links_data]
+        links_html = "\n".join(links)
+        page = page.format(links=links_html)
+
+    out = text.format(page=page)
+
+    with open(f"build/{pagename}.html", "w") as f:
         f.write(out)
